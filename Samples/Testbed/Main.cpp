90d5107f66a9d26d2be24fef4f22dde05ee99dd8
zoogies <ryanzmuda@gmail.com>
2026-05-03 13:09:03 -0400

    switch testbed off and editor on

--- Samples/Testbed/Main.cpp
+++ Samples/Testbed/Main.cpp
@@ -10,38 +10,38 @@ namespace sk = sokoke;
 
 int main()
 {
-    sokoke::Log::Init();
+    // sokoke::Log::Init();
 
-    SK_INFO("Starting Sokoke testbed...");
+    // SK_INFO("Starting Sokoke testbed...");
 
-    sk::backends::sdl::SDLBackend backend;
-    sk::SokokeEngine engine;
+    // sk::backends::sdl::SDLBackend backend;
+    // sk::SokokeEngine engine;
 
-    engine.Initialize(&backend);
+    // engine.Initialize(&backend);
     
-    SK_DEBUG("Hello from Sokoke testbed!");
+    // SK_DEBUG("Hello from Sokoke testbed!");
     
-    auto cube = engine.activeScene.CreateEntity();
-    engine.activeScene.Add<sk::Transform>(cube);
+    // auto cube = engine.activeScene.CreateEntity();
+    // engine.activeScene.Add<sk::Transform>(cube);
 
-    auto& transform = engine.activeScene.Get<sk::Transform>(cube);
-    SK_DEBUG("Initial cube position: ({}, {}, {})", transform.position.x, transform.position.y, transform.position.z);
+    // auto& transform = engine.activeScene.Get<sk::Transform>(cube);
+    // SK_DEBUG("Initial cube position: ({}, {}, {})", transform.position.x, transform.position.y, transform.position.z);
 
-    // example of using a view
-    for (auto [entity, transform] : engine.activeScene.View<sk::Transform>().each()) {
-        SK_DEBUG("Entity {} has position ({}, {}, {})", static_cast<uint32_t>(entity), transform.position.x, transform.position.y, transform.position.z);
-    }
-
-    // engine.activeScene = sk::Scene();
+    // // example of using a view
     // for (auto [entity, transform] : engine.activeScene.View<sk::Transform>().each()) {
     //     SK_DEBUG("Entity {} has position ({}, {}, {})", static_cast<uint32_t>(entity), transform.position.x, transform.position.y, transform.position.z);
     // }
 
-    while(engine.IsRunning())
-    {
-        engine.Tick();
-    }
-    engine.Shutdown();
-    SK_INFO("Goodbye, Sokoke!");
+    // // engine.activeScene = sk::Scene();
+    // // for (auto [entity, transform] : engine.activeScene.View<sk::Transform>().each()) {
+    // //     SK_DEBUG("Entity {} has position ({}, {}, {})", static_cast<uint32_t>(entity), transform.position.x, transform.position.y, transform.position.z);
+    // // }
+
+    // while(engine.IsRunning())
+    // {
+    //     engine.Tick();
+    // }
+    // engine.Shutdown();
+    // SK_INFO("Goodbye, Sokoke!");
     return 0;
 }
